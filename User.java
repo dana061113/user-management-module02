9c47e8d7cab68f003829294d6b827f1ce6200807
dana061113 <danakobylandykyzy@gmail.com>
2026-02-02 21:26:19 +0500

    Add User class with properties and methods

---
+++ User.java
@@ -0,0 +1,23 @@
+public class User {
+    private String name;
+    private String email;
+    private String role;
+
+    public User(String name, String email, String role) {
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
